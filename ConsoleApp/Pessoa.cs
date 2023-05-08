4c2e0b1d4ee0fc6a256b3110dbb24024b8bc75e2
João Bruno <jbrunomf@outlook.com>
2023-05-07 22:13:15 -0400

    atualizando

---
+++ ConsoleApp/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Pessoa
+    {
+        public string? CPF { get; set; }
+        public string? Digito { get; set; }
+        public string? Nome { get; set; }
+        public string? Nascimento { get; set; }
+        public string? Endereco { get; set; }
+        public string? Bairro { get; set; }
+        public string? Cidade { get; set; }
+        public string? UF { get; set; }
+        public string? CEP { get; set; }
+        public string? DDD { get; set; }
+        public string? Telefone { get; set; }
+
+        public Pessoa(string? cpf, string? digito, string? nome, string? nascimento, string? endereco, string? bairro, string? cidade, string? uf, string? cep, string? ddd, string? telefone)
+        {
+            CPF = cpf;
+            Digito = digito;
+            Nome = nome;
+            Nascimento = nascimento;
+            Endereco = endereco;
+            Bairro = bairro;
+            Cidade = cidade;
+            UF = uf;
+            CEP = cep;
+            DDD = ddd;
+            Telefone = telefone;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"Nome: {Nome}, CPF: {CPF}, Digito: {Digito}, Nascimento: {Nascimento}, Endereço: {Endereco}, Bairro: {Bairro}, Cidade: {Cidade}, UF: {UF}" +
+                $"CEP: {CEP}, DDD: {DDD}, Telefone: {Telefone}";
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,52 +1,91 @@
-﻿using System.IO;
+﻿using System.Data;
+using System.IO;
 using DbfDataReader;
 using System.Data.Odbc;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using ConsoleApp;
+using Microsoft.Office.Interop.Outlook;
+using NUnit.Framework.Internal;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        // Abrir a conexão com o arquivo MT.dbf
-        var stream = new FileStream(@"C:\\ProjetoC#\\Dinho\\BancoDados\\MT.DBF", FileMode.Open);
-        var reader = new DBFReader(stream);
-
-        // Ler o primeiro registro do arquivo
-        reader.Read();
-
-        // Obter os valores do CPF e data de nascimento do registro
-        string cpf = reader.GetString("CPF");
-        string dataNascimento = reader.GetDateTime("DT_NASC").ToString("ddMMyyyy");
-
-        // Fechar a conexão com o arquivo
-        reader.Dispose();
-        stream.Dispose();
-
         // Configurar o driver do Selenium
         var options = new ChromeOptions();
         options.AddArgument("--start-maximized");
         // var driver = new ChromeDriver(options);
-        var driver = new ChromeDriver(@"C:\ChromeDriver");
+        var driver = new EdgeDriver(@"C:\Webdriver\msedgedriver.exe");
+
+        var dbfPath = "C:\\Users\\jbrun\\Downloads\\ProjetoC#\\ProjetoC#\\Dinho\\BancoDados\\MT.dbf";
+        var skipDeleted = true;
+        List<string> valores = new List<string>();
+
+        using (var dbfTable = new DbfTable(dbfPath, Encoding.UTF8))
+        {
+            var dbfRecord = new DbfRecord(dbfTable);
+
+            while (dbfTable.Read(dbfRecord))
+            {
+                if (skipDeleted && dbfRecord.IsDeleted)
+                {
+                    continue;
+                }
+
+
+                //foreach (var dbfValue in dbfRecord.Values)
+                //{
+                //    var teste = dbfValue.GetValue();
+                //}
+
+                Pessoa p = new Pessoa(dbfRecord.GetValue(0).ToString(), dbfRecord.GetValue(1).ToString(),
+                    dbfRecord.GetValue(2).ToString(),
+                    dbfRecord.GetValue(3).ToString(), dbfRecord.GetValue(4).ToString(),
+                    dbfRecord.GetValue(5).ToString(), dbfRecord.GetValue(6).ToString(),
+                    dbfRecord.GetValue(7).ToString(), dbfRecord.GetValue(8).ToString(),
+                    dbfRecord.GetValue(9).ToString(), dbfRecord.GetValue(10).ToString());
+
+                Console.WriteLine($"{p}\n");
+                break;
+            }
+
+        }
 
 
         // Navegar até a página de login
         driver.Navigate().GoToUrl("https://cnp3.consorciocanopus.com.br/Newcon.AutoAtendimento.Web/valor-receber");
 
         // Preencher o campo de CPF
-        var campoCpf = driver.FindElementById("inscricaoNacional");
-        campoCpf.SendKeys(cpf);
+        var campo = GetCpfInputWebElement();
+        campo.Click();
+        Thread.Sleep(1000);
 
-        // Preencher o campo de data de nascimento
-        var campoDataNascimento = driver.FindElementById("dataNascimentoFundacao");
-        campoDataNascimento.SendKeys(dataNascimento);
-
-        // Submeter o formulário
-        var botaoEntrar = driver.FindElementByXPath("//button[contains(., 'Entrar')]");
-        botaoEntrar.Click();
+        var campoNascimento = GetNascimentoInputWebElement();
+        campoNascimento.Click();
+        Thread.Sleep(1000);
 
         // Fechar o driver
         driver.Quit();
+
+
+        IWebElement GetCpfInputWebElement()
+        {
+            var campoCPF= new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("html.h100 body#block-sub-menu.h100 div#root.h100 div main.sc-bxivhb.lhSCcI div.box-login.sc-ifAKCX.iymHTE form.ant-form.ant-form-horizontal.nw-form.sc-EHOje.edRFCv div.ant-row.ant-form-item.nw-form-item.sc-bZQynM.bCqhlb div.ant-form-item-control-wrapper div.ant-form-item-control span.ant-form-item-children input#inscricaoNacional.ant-input.nw-input.sc-dnqmqq.cSUyVI")));
+            return campoCPF;
+        }
+
+        IWebElement GetNascimentoInputWebElement()
+        {
+            var campo = new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("html.h100 body#block-sub-menu.h100 div#root.h100 div main.sc-bxivhb.lhSCcI div.box-login.sc-ifAKCX.iymHTE form.ant-form.ant-form-horizontal.nw-form.sc-EHOje.edRFCv div.ant-row.ant-form-item.nw-form-item.sc-bZQynM.bCqhlb div.ant-form-item-control-wrapper div.ant-form-item-control span.ant-form-item-children input#dataNascimentoFundacao.ant-input.nw-input.sc-dnqmqq.cSUyVI")));
+            return campo;
+        }
     }
+
 }
+
